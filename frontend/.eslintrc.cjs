0fb2266f070589735d9e1d74084ddeac97e95af5
juni <woajun@naver.com>
2023-01-30 09:54:21 +0900

    init frontend

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    '@vue/airbnb',
+    'airbnb-typescript/base',
+    '@vue/typescript/recommended',
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    project: './frontend/tsconfig.json',
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'linebreak-style': 'off',
+    'vuejs-accessibility/form-control-has-label': 'off',
+    'vue/html-button-has-type': 'off',
+    'vuejs-accessibility/label-has-for': 'off',
+  },
+  ignorePatterns: ['dist/**', 'debug.js', 'tailwind.config.js'],
+};
---
+++ frontend/src/App.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>Fitness Manager</div>
+</template>

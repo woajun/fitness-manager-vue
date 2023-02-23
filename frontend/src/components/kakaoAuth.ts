2622b380a2c81d47467fcc2b36846edf33d4721f
juni <woajun@naver.com>
2023-02-23 13:56:59 +0900

    remove login at frontend

--- frontend/src/components/kakaoAuth.ts
+++ frontend/src/components/kakaoAuth.ts
@@ -1,17 +1,9 @@
 import axios from 'axios';
 
-declare const Kakao : {
-  init: (key: string) => void;
-  Auth: any;
-};
-
-export function kakaoInit() {
-  Kakao.init('8e53e038d8115c2ce57ff4702ca30d40');
-}
-
 async function login() {
-  const apple = await axios.get('/login/kakao');
-  location.replace(apple.data);
+  const loginURL = await axios.get('/login/kakao');
+  alert('hello');
+  window.location.replace(loginURL.data);
 }
 
 export default {
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 import router from './router';
 import App from './App.vue';
 import './style.css';
-import {kakaoInit} from '@/components/kakaoAuth';
 
 const baseURL = 'http://localhost:5000/';
 // const baseURL = 'http://fitnessfriend-env.eba-ieugjpqk.ap-northeast-2.elasticbeanstalk.com/';
@@ -14,5 +13,4 @@ axios.defaults.headers.common['Content-Type'] = 'application/json;charset=UTF-8'
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 const app = createApp(App);
 app.use(router);
-kakaoInit();
 app.mount('#app');

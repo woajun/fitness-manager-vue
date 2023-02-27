16704bdad7de196aa7a5be6e75b68adb5e77bc3e
juni <woajun@naver.com>
2023-02-27 15:33:42 +0900

    airbnb eslinting

---
+++ mobile/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+  ],
+  overrides: [
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'linebreak-style': [
+      'off',
+      'windows',
+    ],
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'react/jsx-filename-extension': 'off',
+  },
+};
--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,23 +1,16 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Timer from "./pages/timer/Timer";
+import React from 'react';
+import { Text, View } from 'react-native';
+import Timer from './pages/timer/Timer';
 
 export default function App() {
   return (
     <View>
       <Text>
         Hello, TI am your cat!
-        aasdasdasdsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+        aassdsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
       </Text>
       {/* <MyWebView /> */}
-      <Timer></Timer>
+      <Timer />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    width: "100%",
-    height: "100%",
-  },
-});

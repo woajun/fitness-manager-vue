01a5bd46cbeca7281eac1c81ee20d5173e050cc4
juni <woajun@naver.com>
2023-02-27 14:46:33 +0900

    add airbnb eslint

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,5 @@
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import MyWebView from "./components/MyWebView";
 import Timer from "./pages/timer/Timer";
 
 export default function App() {

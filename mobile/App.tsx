bf917dba1484b5eb965f90063cf080161f14b124
juni <woajun@naver.com>
2023-02-28 17:41:14 +0900

    add Pressable Button

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Text, Pressable } from 'react-native';
+import {
+  View, Text, Pressable,
+} from 'react-native';
 import tw from 'twrnc';
 import { useAppColorScheme } from 'twrnc/dist/esm/hooks';
 
@@ -8,11 +10,15 @@ export default function App() {
 
   return (
     <Pressable
-      onPress={toggleColorScheme}
       style={tw`flex-1 items-center justify-center dark:bg-slate-800`}
     >
       <View>
-        <Text style={tw`dark:text-white text-2xl`}>{`Try clicking me! ${colorScheme === 'dark' ? '🌙' : '🌞'}`}</Text>
+        <Pressable
+          style={tw`rounded-lg bg-slate-700`}
+          onPress={toggleColorScheme}
+        >
+          <Text style={tw`text-slate-50 dark:text-white text-2xl p-3 text-center`}>{`Try clicking me! ${colorScheme === 'dark' ? '🌙' : '🌞'}`}</Text>
+        </Pressable>
       </View>
     </Pressable>
   );

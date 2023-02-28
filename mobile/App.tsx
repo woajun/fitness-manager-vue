7e8f064ee07c86e1e76aa09fa9f713672b7ab1a9
juni <woajun@naver.com>
2023-02-28 17:48:26 +0900

    add JButton.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import {
-  View, Text, Pressable,
+  View, Pressable,
 } from 'react-native';
 import tw from 'twrnc';
-import { useAppColorScheme } from 'twrnc/dist/esm/hooks';
+import JButton from './components/JButton';
 
 export default function App() {
-  const [colorScheme, toggleColorScheme] = useAppColorScheme(tw);
-
   return (
     <Pressable
       style={tw`flex-1 items-center justify-center dark:bg-slate-800`}
     >
       <View>
-        <Pressable
-          style={tw`rounded-lg bg-slate-700`}
-          onPress={toggleColorScheme}
-        >
-          <Text style={tw`text-slate-50 dark:text-white text-2xl p-3 text-center`}>{`Try clicking me! ${colorScheme === 'dark' ? '🌙' : '🌞'}`}</Text>
-        </Pressable>
+        <JButton />
       </View>
     </Pressable>
   );
--- mobile/components/MyWebView.tsx
+++ mobile/components/MyWebView.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { WebView } from 'react-native-webview';
 
-const MyWebView = () => {
-    return (
-      <WebView
-        source={{ uri: 'https://symphonious-eclair-024f7d.netlify.app/' }}
-      />
-    );
+function MyWebView() {
+  return (
+    <WebView
+      source={{ uri: 'https://symphonious-eclair-024f7d.netlify.app/' }}
+    />
+  );
 }
 export default MyWebView;

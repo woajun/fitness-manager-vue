17243650ae7675fb93fc3813121c6b2c73450f08
juni <woajun@naver.com>
2023-04-07 14:16:18 +0900

    add excecises

--- frontend/src/data/excercises.ts
+++ frontend/src/data/excercises.ts
@@ -6,8 +6,14 @@ const excercises = [
   { exrId: 4, exrName: '팔굽혀펴기 (푸시업)' },
   { exrId: 5, exrName: '스쿼트' },
   { exrId: 6, exrName: '데드리프트' },
-  { exrId: 7, exrName: '번지피지오' },
-  { exrId: 8, exrName: 'AB슬라이드' },
-  { exrId: 9, exrName: '악력기' },
+  { exrId: 7, exrName: '케이블 푸쉬다운' },
+  { exrId: 8, exrName: '시티드 로우' },
+  { exrId: 10, exrName: '사이드 레터럴 레이즈' },
+  { exrId: 11, exrName: '벤트오버 레터럴 레이즈' },
+  { exrId: 12, exrName: '바벨컬' },
+  { exrId: 13, exrName: '덤벨컬' },
+  { exrId: 14, exrName: '밀리터리 프레스' },
+  { exrId: 15, exrName: '케이블 리버스 플라이' },
+  { exrId: 16, exrName: '악력기' },
 ];
 export default excercises;

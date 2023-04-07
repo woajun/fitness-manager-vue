a6a9c468157d4c98fefa9c5808fc8fe7c7e35d68
juni <woajun@naver.com>
2023-04-07 14:17:13 +0900

    add excercises

--- frontend/src/data/excercises.ts
+++ frontend/src/data/excercises.ts
@@ -9,7 +9,7 @@ const excercises = [
   { exrId: 6, exrName: '데드리프트' },
   { exrId: 7, exrName: '케이블 푸쉬다운' },
   { exrId: 8, exrName: '시티드 로우' },
-  { exrId: 10, exrName: '사이드 레터럴 레이즈' },
+  { exrId: 17, exrName: '사이드 레터럴 레이즈' },
   { exrId: 11, exrName: '벤트오버 레터럴 레이즈' },
   { exrId: 12, exrName: '바벨컬' },
   { exrId: 13, exrName: '덤벨컬' },

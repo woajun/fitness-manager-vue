f4a44dceb345411c563a2cbdcec9be7f1b47eb88
juni <woajun@naver.com>
2023-04-11 16:20:17 +0900

    add PreviewLayout

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { ScrollView } from 'react-native';
 import Apple from './pages/timer/Apple';
 import Timer from './pages/timer/Timer';
+import PreviewLayout from './pages/PreviewLayout';
 
 export default function App() {
   return (
     <ScrollView>
       <Apple />
       <Timer />
+      <PreviewLayout />
     </ScrollView>
   );
 }

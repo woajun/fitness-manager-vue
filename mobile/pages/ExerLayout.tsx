92ae81de5244f9846e08723bbdcd101819784c58
juni <woajun@naver.com>
2023-04-12 13:22:27 +0900

    add stopwatch

--- mobile/pages/ExerLayout.tsx
+++ mobile/pages/ExerLayout.tsx
@@ -1,30 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Alert, Text, View,
 } from 'react-native';
 import MyButton from '../components/MyButton';
-import StopWatch from './stopWatch';
 
 export default function ExcerLayout() {
-  let presentTimeText = 0;
-  let isRun = false;
-  const timer = new StopWatch(
-    (ms) => {
-      console.log(ms);
-      presentTimeText = ms;
-    },
-    (bool) => { isRun = bool; },
-  );
+  const [startTime, setStartTime] = useState(0);
+  const [now, setNow] = useState(0);
+
+  function handleStart() {
+    // Start counting.
+    setStartTime(Date.now());
+    setNow(Date.now());
+
+    setInterval(() => {
+      // Update the current time every 10ms.
+      setNow(Date.now());
+    }, 10);
+  }
+
+  let secondsPassed = 0;
+  if (startTime != null && now != null) {
+    secondsPassed = (now - startTime) / 1000;
+  }
 
   return (
     <View style={{ flex: 1, flexDirection: 'column', padding: 20 }}>
       <View style={{ flex: 1, justifyContent: 'center', backgroundColor: 'aqua' }} />
       <View style={{ flex: 2, justifyContent: 'center', flexDirection: 'row' }}>
         <View style={{ flex: 1 }}>
-          <Text>{presentTimeText}</Text>
+          <Text>{secondsPassed.toFixed(3)}</Text>
         </View>
         <View style={{ flex: 1, backgroundColor: 'blue' }}>
-          <Text>{ isRun }</Text>
+          <Text>{ }</Text>
         </View>
       </View>
       <View style={{ flex: 3, justifyContent: 'center', backgroundColor: 'green' }} />
@@ -38,7 +46,7 @@ export default function ExcerLayout() {
           <MyButton
             style={{ flex: 1, marginRight: 1 }}
             label="start"
-            onPress={() => timer.start()}
+            onPress={() => handleStart()}
           />
           <MyButton
             style={{ flex: 1, marginLeft: 1 }}

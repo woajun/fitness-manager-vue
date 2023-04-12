e34c9d3945fe0967d0cc4f16d67b20d86f962b49
juni <woajun@naver.com>
2023-04-12 10:40:43 +0900

    justifyContent

--- mobile/pages/ExerLayout.tsx
+++ mobile/pages/ExerLayout.tsx
@@ -1,33 +1,32 @@
 import React from 'react';
 import {
-  Alert, Button, Pressable, Text, View,
+  Alert, View,
 } from 'react-native';
-import tw from 'twrnc';
 import MyButton from '../components/MyButton';
 
 export default function ExcerLayout() {
   return (
     <View style={{ flex: 1, flexDirection: 'column', padding: 20 }}>
-      <View style={{ flex: 1, backgroundColor: 'aqua' }} />
-      <View style={{ flex: 2, flexDirection: 'row' }}>
+      <View style={{ flex: 1, justifyContent: 'center', backgroundColor: 'aqua' }} />
+      <View style={{ flex: 2, justifyContent: 'center', flexDirection: 'row' }}>
         <View style={{ flex: 1 }} />
         <View style={{ flex: 1, backgroundColor: 'blue' }} />
       </View>
-      <View style={{ flex: 3, backgroundColor: 'green' }} />
-      <View style={{ flex: 2, backgroundColor: 'yellow' }} />
-      <View style={{ flex: 2, backgroundColor: 'aqua' }} />
-      <View style={{ flex: 2, backgroundColor: 'white' }}>
+      <View style={{ flex: 3, justifyContent: 'center', backgroundColor: 'green' }} />
+      <View style={{ flex: 2, justifyContent: 'center', backgroundColor: 'yellow' }} />
+      <View style={{ flex: 2, justifyContent: 'center', backgroundColor: 'aqua' }} />
+      <View style={{ flex: 2, justifyContent: 'center', backgroundColor: 'white' }}>
         <View style={{
           flexDirection: 'row',
         }}
         >
           <MyButton
-            style={{ flex: 1, paddingRight: 1 }}
+            style={{ flex: 1, marginRight: 1 }}
             label="start"
             onPress={() => Alert.alert('Start button pressed')}
           />
           <MyButton
-            style={{ flex: 1, paddingLeft: 1 }}
+            style={{ flex: 1, marginLeft: 1 }}
             label="end"
             onPress={() => Alert.alert('End button pressed')}
           />

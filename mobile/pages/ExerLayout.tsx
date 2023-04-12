7180073ae08a50f9246d001878b6b0bddb69d50b
juni <woajun@naver.com>
2023-04-12 09:22:17 +0900

    fix layout

--- mobile/pages/ExerLayout.tsx
+++ mobile/pages/ExerLayout.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-  },
-});
+import { View } from 'react-native';
 
 export default function ExcerLayout() {
   return (
-    <View
-      style={[
-        styles.container,
-        {
-          flexDirection: 'row',
-        },
-      ]}
-    >
-      <View style={{ flex: 1, backgroundColor: 'red' }} />
-      <View style={{ flex: 1, backgroundColor: 'blue' }} />
+    <View style={{ flex: 1, flexDirection: 'column' }}>
+      <View style={{ flex: 1, backgroundColor: 'aqua' }} />
+      <View style={{ flex: 2, flexDirection: 'row' }}>
+        <View style={{ flex: 1, backgroundColor: 'red' }} />
+        <View style={{ flex: 1, backgroundColor: 'blue' }} />
+      </View>
+      <View style={{ flex: 3, backgroundColor: 'green' }} />
+      <View style={{ flex: 2, backgroundColor: 'yellow' }} />
+      <View style={{ flex: 2, backgroundColor: 'aqua' }} />
+      <View style={{ flex: 2, backgroundColor: 'white' }} />
     </View>
   );
 }

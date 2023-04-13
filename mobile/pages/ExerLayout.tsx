b99b2aaf08a31207ea6f042884f4446f810c2489
juni <woajun@naver.com>
2023-04-13 16:50:54 +0900

    extract stopwatch

--- mobile/pages/ExerLayout.tsx
+++ mobile/pages/ExerLayout.tsx
@@ -2,45 +2,7 @@ import React, { useState, useRef } from 'react';
 import { Text, View } from 'react-native';
 import MyButton from '../components/MyButton';
 import Timer from './timer/Timer';
-
-class Stopwatch {
-  setTime: React.Dispatch<React.SetStateAction<number>>;
-  #startTime = 0;
-  #intervalID = 0;
-  #keepTime = 0;
-
-  constructor(setTime: any) {
-    this.setTime = setTime;
-  }
-
-  getTime() {
-    return this.#keepTime + (Date.now() - this.#startTime);
-  }
-
-  run() {
-    this.#startTime = Date.now();
-
-    clearInterval(this.#intervalID);
-    this.#intervalID = setInterval(() => {
-      this.setTime(this.getTime());
-    }, 10)
-  }
-
-  pause() {
-    this.#keepTime = this.getTime();
-    clearInterval(this.#intervalID);
-  }
-
-  reset() {
-    const result = this.getTime();
-    clearInterval(this.#intervalID);
-    this.#startTime = Date.now();
-    this.#intervalID = 0;
-    this.#keepTime = 0;
-    this.setTime(this.getTime());
-    return result;
-  }
-}
+import Stopwatch from './stopwatch';
 
 export default function ExcerLayout() {
   const [time, setTime] = useState(0);
---
+++ mobile/pages/stopwatch.ts
@@ -0,0 +1,38 @@
+export default class Stopwatch {
+  setTime: React.Dispatch<React.SetStateAction<number>>;
+  #startTime = 0;
+  #intervalID = 0;
+  #keepTime = 0;
+
+  constructor(setTime: any) {
+    this.setTime = setTime;
+  }
+
+  getTime() {
+    return this.#keepTime + (Date.now() - this.#startTime);
+  }
+
+  run() {
+    this.#startTime = Date.now();
+
+    clearInterval(this.#intervalID);
+    this.#intervalID = setInterval(() => {
+      this.setTime(this.getTime());
+    }, 10)
+  }
+
+  pause() {
+    this.#keepTime = this.getTime();
+    clearInterval(this.#intervalID);
+  }
+
+  reset() {
+    const result = this.getTime();
+    clearInterval(this.#intervalID);
+    this.#startTime = Date.now();
+    this.#intervalID = 0;
+    this.#keepTime = 0;
+    this.setTime(this.getTime());
+    return result;
+  }
+}

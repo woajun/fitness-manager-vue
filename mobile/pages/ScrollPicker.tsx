b773302790e295da3d45dac668c5c80ae6b7de2e
juni <woajun@naver.com>
2023-04-17 15:49:25 +0900

    add SelectedItem

--- mobile/pages/ScrollPicker.tsx
+++ mobile/pages/ScrollPicker.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/require-default-props */
 /* eslint react/no-unused-prop-types: "warn" */
 /* eslint-disable react/destructuring-assignment */
 import React, { useRef } from 'react';
@@ -7,7 +8,14 @@ import {
   NativeScrollEvent, NativeSyntheticEvent, ScrollView, Text, View,
 } from 'react-native';
 
-type ScrollPickerProps = {
+type ContainerProps = {
+  wrapperHeight: number,
+  wrapperWidth: number,
+  wrapperBackground: string,
+  children: any,
+}
+
+type ScrollPickerProps = Partial<ContainerProps> & {
   style: any,
   dataSource?: any[],
   selectedIndex: number,
@@ -15,9 +23,6 @@ type ScrollPickerProps = {
   renderItem: () => void,
   highlightColor?: string,
   itemHeight?: number,
-  wrapperBackground?: string,
-  wrapperWidth?: number,
-  wrapperHeight?: number,
   highlightWidth?: number,
   highlightBorderWidth?: number,
   itemTextStyle?: any,
@@ -26,37 +31,85 @@ type ScrollPickerProps = {
   onScrollEndDrag?: () => void,
 }
 
-export default function ScrollPicker(props : ScrollPickerProps) {
+function Container(props: ContainerProps) {
   return (
     <View style={{
       flex: 1,
       overflow: 'hidden',
       alignSelf: 'center',
       height: props.wrapperHeight,
-      width: props.wrapperWidth,
       backgroundColor: props.wrapperBackground,
     }}
-    />
+    >
+      {props.children}
+    </View>
+  );
+}
+
+function SelectedItem({ children, itemHeight }: {children: any, itemHeight: number}) {
+  return (
+    <View style={{
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: itemHeight,
+    }}
+    >
+      {children}
+    </View>
+  );
+}
+export default function ScrollPicker({
+  wrapperHeight = 180,
+  wrapperWidth = 150,
+  wrapperBackground = '#FFFFFF',
+  dataSource = ['00', '10', '20'],
+  itemTextStyle = {
+    fontSize: 20, lineHeight: 26, textAlign: 'center', color: '#B4B4B4',
+  },
+  activeItemTextStyle = {
+    fontSize: 20, lineHeight: 26, textAlign: 'center', color: '#222121',
+  },
+  itemHeight = 30,
+} : ScrollPickerProps) {
+  const sview = useRef(null);
+  const sltIdx = useRef(0);
+
+  function onMomentumScrollBegin() {
+
+  }
+
+  return (
+    <Container
+      wrapperHeight={wrapperHeight}
+      wrapperWidth={wrapperWidth}
+      wrapperBackground={wrapperBackground}
+    >
+      <ScrollView
+        ref={sview}
+        bounces={false}
+        showsVerticalScrollIndicator={false}
+        onMomentumScrollBegin={onMomentumScrollBegin}
+      >
+        {dataSource.map((d, i) => {
+          const isSelected = i === sltIdx.current;
+          const item = <Text style={isSelected ? activeItemTextStyle : itemTextStyle}>{d}</Text>;
+          return (
+            <SelectedItem itemHeight={itemHeight} key={d}>
+              {item}
+            </SelectedItem>
+          );
+        })}
+      </ScrollView>
+    </Container>
   );
 }
 
 ScrollPicker.defaultProps = {
-  dataSource: [1, 2, 3],
-  itemHeight: 60,
-  wrapperBackground: '#FFFFFF',
-  wrapperHeight: 180,
-  wrapperWidth: 150,
   highlightWidth: Dimensions.get('window').width,
   highlightBorderWidth: 2,
   highlightColor: '#333',
   onMomentumScrollEnd: () => {
   },
   onScrollEndDrag: () => {
-  },
-  itemTextStyle: {
-    fontSize: 20, lineHeight: 26, textAlign: 'center', color: '#B4B4B4',
-  },
-  activeItemTextStyle: {
-    fontSize: 20, lineHeight: 26, textAlign: 'center', color: '#222121',
   },
 };

23d20f09269c8c2d44f591cc1b14c1aeb05f15e0
juni <woajun@naver.com>
2023-04-18 10:50:51 +0900

    remove chatCptScrollPicker

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ExcerLayout from './pages/ExerLayout';
-import ChatGtpScrollPicker from './pages/ChatGptScrollPicker';
 
 export default function App() {
   return (
-    <ChatGtpScrollPicker />
-    // <ExcerLayout />
+    <ExcerLayout />
   );
 }

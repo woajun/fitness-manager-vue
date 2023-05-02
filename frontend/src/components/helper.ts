7439f13aa68896e71778fc6d28989d5d9b346d67
woajun <woajun@naver.com>
2023-05-02 21:25:45 +0900

    fix time text

--- frontend/src/components/helper.ts
+++ frontend/src/components/helper.ts
@@ -2,16 +2,19 @@
 export function to00(r: number) {
   return r < 10 ? `0${r}` : `${r}`;
 }
+export function to0(r: number) {
+  return r < 10 ? `${r}` : `${r}`;
+}
 
 function positive(ms:number) {
   const rawH = ms / 1000 / 60 / 60;
   const h = Math.floor(rawH);
   const m = Math.floor((rawH - h) * 60);
   const s = Math.floor(((rawH - h) * 60 - m) * 60);
-  const mSec = Math.floor((ms % 1000) / 10);
+  const mSec = Math.floor((ms % 1000) / 100);
 
-  return rawH >= 1 ? `${to00(h)}:${to00(m)}:${to00(s)}`
-    : `${to00(m)}:${to00(s)}:${to00(mSec)}`;
+  return rawH >= 1 ? `${to00(h)}:${to00(m)}.${to0(s)}`
+    : `${to00(m)}:${to00(s)}.${to0(mSec)}`;
 }
 
 function negative(ms: number) {

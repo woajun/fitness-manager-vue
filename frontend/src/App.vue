138246f66dc9ba63383b1db666b1f80e07795c15
woajun <woajun@naver.com>
2023-05-06 00:22:13 +0900

    80vh

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,7 +15,7 @@ import NavBar from './pages/navigation/NavBar.vue';
 <style scoped>
 .app-container {
   width: 100%;
-  height: 90vh;
+  height: 80vh;
   display: flex;
   flex-direction: column;
   position: fixed;
